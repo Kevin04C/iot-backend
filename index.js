e7db1f2b463e523a8e63ce4af278f7f6b99bbd24
Kevin04C <kevincespedes2004@gmail.com>
2024-10-28 19:32:01 -0500

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { config } from 'dotenv';
 config();
 
-const PORT = environment.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
