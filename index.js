8c121ba6ed2a69a04dc899ffc9b40de45d36af10
Kevin04C <kevincespedes2004@gmail.com>
2024-10-29 12:52:31 -0500

    hello

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ app.get('/ping', (req, res) => {
   res.json({ message: 'pong' });
 });
 
+app.get('/', (req, res) => {
+  res.json({ message: 'hello' });
+});
+
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT + "");
 })
